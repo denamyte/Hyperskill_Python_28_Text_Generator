e2b2328133a0c8775ad7bb107b35b7e485c7b93c
denys <denamyte@gmail.com>
2023-02-21 12:47:42 +0200

    2023-02-21: Stage 3/5: finished

--- Text Generator/task/text_generator/data.py
+++ Text Generator/task/text_generator/data.py
@@ -1,10 +1,13 @@
-from dataclasses import dataclass
+from collections import Counter
+from typing import List
 
 
-@dataclass
-class Bigram:
-    head: str
-    tail: str
+class BigramCounter:
+    def __init__(self, head, tails: List[str]):
+        self._head = head
+        self._counter = Counter(tails)
 
     def __str__(self):
-        return f'Head: {self.head} Tail: {self.tail}'
+        return f'Head: {self._head}\n' +\
+                '\n'.join(f'Tail: {w:10s} Count: {c}' for w, c
+                          in self._counter.most_common()) + '\n'
--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,19 +1,18 @@
 import re
-from typing import List
-from data import Bigram
-
-RE_ERROR = re.compile(r'(?<=\')\w+(?=Error)')
+from collections import defaultdict
+from typing import List, Dict
+from data import BigramCounter
 
 
 def main():
     corpus = read_file(input())
     bigrams = convert_to_bigrams(corpus)
-    print(f'Number of bigrams: {len(bigrams)}\n')
     while (cmd := input()) != 'exit':
         try:
-            print(bigrams[int(cmd)])
-        except Exception as e:
-            print(f'{RE_ERROR.search(str(type(e))).group()} Error ' * 2)
+            print(bigrams[cmd])
+        except KeyError:
+            print(f'Head: {cmd}')
+            print('The requested word is not in the model.')
 
 
 def read_file(name: str) -> List[str]:
@@ -22,10 +21,13 @@ def read_file(name: str) -> List[str]:
     return re.split(r'\s', text.strip())
 
 
-def convert_to_bigrams(corpus: List[str]) -> List[Bigram]:
+def convert_to_bigrams(corpus: List[str]) -> Dict[str, BigramCounter]:
     second_iter = iter(corpus)
     next(second_iter)
-    return [Bigram(*z) for z in zip(corpus, second_iter)]
+    dic = defaultdict(list)
+    for h, t in zip(corpus, second_iter):
+        dic[h].append(t)
+    return {k: BigramCounter(k, v) for k, v in dic.items()}
 
 
 if __name__ == '__main__':
